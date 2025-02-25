af666c5eeb9194d3a0d769493bff88f4b3ec0e53
Kuralarasan N <137192990+kuralarasan-n@users.noreply.github.com>
2025-02-25 16:18:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ if ingredients_list:
             values ('""" +ingredients_string+ """', '"""+name_on_order+"""')"""
 
     st.write(my_insert_stmt)
-    st.stop()
+    # st.stop()
     
     time_to_insert = st.button ('Submit Order')
 
