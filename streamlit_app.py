13bf45df83bed12595c2d075b1b93a6302bc9778
Kuralarasan N <137192990+kuralarasan-n@users.noreply.github.com>
2025-03-17 05:14:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,15 +41,17 @@ if ingredients_list:
 
     # st.write(my_insert_stmt)
     # st.stop()
-    
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
+
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' +name_on_order+ '!', icon="✅")
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+
 
     
 
